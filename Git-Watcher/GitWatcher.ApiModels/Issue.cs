b11f8059eda60417ac18142c2e83e32b87a7eee5
sINFdapiot <Daniel.Piotrowski@stud.fh-rosenheim.de>
2019-06-26 22:56:03 +0200

    moved stuff

--- Git-Watcher/GitWatcher.ApiModels/Issue.cs
+++ Git-Watcher/GitWatcher.ApiModels/Issue.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
 namespace Git_Watcher_Client.Models
 {
     public class Issue
@@ -18,5 +11,6 @@ namespace Git_Watcher_Client.Models
         public string Name { get; set; }
         public DateTime Created { get; set; }
         public string Link { get; set; }
+        public bool Important { get; set; }
     }
 }
